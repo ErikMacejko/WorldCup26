58b5273705bd3a3ed60ac91c913cff2c1d99c66f
ericko <erik.hockeyarena@gmail.com>
2026-06-12 15:06:43 +0200

    Log resolved MONGO_URI host on startup to debug Railway env var injection

--- server/src/db.js
+++ server/src/db.js
@@ -3,6 +3,10 @@ import { config } from './config.js';
 
 export async function connectDb() {
   mongoose.set('strictQuery', true);
+  console.log(
+    '[db] connecting using MONGO_URI =',
+    config.mongoUri.replace(/\/\/[^@]*@/, '//<redacted>@')
+  );
   await mongoose.connect(config.mongoUri);
   console.log('[db] connected to MongoDB');
   return mongoose.connection;
